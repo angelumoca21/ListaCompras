d6a2415ba2b1d9660a0332cf11ed8007ccb8a7b1
angelumoca21 <angelmoca21@gmail.com>
2022-04-03 16:30:14 -0500

    Impresion de vectores

--- index.js
+++ index.js
@@ -6,31 +6,28 @@ function agregarItem(){
     var inputTexto = document.getElementById("cajaTexto");
     var texto = inputTexto.value;
     console.log(texto);
+
     var inputCategoria = document.getElementById("opciones");
     var categoria = inputCategoria.value;
     console.log(categoria);
-    if(categoria == "verdurasFrutas"){
-        verdurasFrutas.push(texto);
-    }
-    else if(categoria == "carnes"){
-        carnes.push(texto);
-    }
-    else if(categoria == "bebidas"){
-        bebidas.push(texto);
-    }
+
     var textoVerdurasFrutas = document.getElementById("listaFrutasVerduras");
     var textoCarnes = document.getElementById("listaCarnes");
     var textoBebidas = document.getElementById("listaBebidas");
-    for(var i = 0;i < verdurasFrutas.length;i++)
-    {
-        textoVerdurasFrutas.innerText = verdurasFrutas[i];
-    }
-    for(var i = 0;i < carnes.length;i++)
-    {
-        textoCarnes.innerText = carnes[i];
-    }
-    for(var i = 0;i < bebidas.length;i++)
-    {
-        textoBebidas.innerText = bebidas[i];
-    }
+
+    if(categoria == "verdurasFrutas")
+        verdurasFrutas.push(texto);
+    else if(categoria == "carnes")
+        carnes.push(texto);
+    else if(categoria == "bebidas")
+        bebidas.push(texto);
+
+    textoVerdurasFrutas.innerHTML = "";
+    verdurasFrutas.forEach(item => textoVerdurasFrutas.innerHTML += `<li>${item}</li>`);
+
+    textoCarnes.innerHTML = "";
+    carnes.forEach(item => textoCarnes.innerHTML += `<li>${item}</li>`);
+
+    textoBebidas.innerHTML = "";
+    bebidas.forEach(item => textoBebidas.innerHTML += `<li>${item}</li>`);
 }
